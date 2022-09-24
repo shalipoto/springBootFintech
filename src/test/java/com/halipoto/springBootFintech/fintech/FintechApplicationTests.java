717b68f72dc8758b2cc30c46cad18ee1b4c9505a
Saleem Halipoto <shalipoto@gmail.com>
2022-09-23 20:27:55 -0500

    inital commit

---
+++ src/test/java/com/halipoto/springBootFintech/fintech/FintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halipoto.springBootFintech.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
