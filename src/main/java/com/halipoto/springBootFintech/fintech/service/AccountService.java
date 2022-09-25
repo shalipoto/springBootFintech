09d03c3f2b59a99d965321a41cac185611bfdd34
Saleem Halipoto <shalipoto@gmail.com>
2022-09-24 22:25:17 -0500

    Created method bare bones starting point, added a TODO

--- src/main/java/com/halipoto/springBootFintech/fintech/service/AccountService.java
+++ src/main/java/com/halipoto/springBootFintech/fintech/service/AccountService.java
@@ -1,9 +1,24 @@
 package com.halipoto.springBootFintech.fintech.service;
 
+import com.halipoto.springBootFintech.fintech.model.CardAcct;
+import com.halipoto.springBootFintech.fintech.model.CcAccount;
+
 public class AccountService {
 
-    public processCreditCardAccountForUi(CcAccount cc, CardAcct cardAcct){
+    /**
+     * This method receives a CcAccount retreived from the backend
+     * and then populates the ui card account object with synthetic data
+     * @param cc
+     * @return cardAcct
+     */
+    public CardAcct processCreditCardAccountForUi(CcAccount cc){
 
+        CardAcct cardAcct = new CardAcct();
+
+        // TODO
+        // take the input CcAccount object, create values and add to the
+        // newly created CardAcct object and return the object.
+        // you are only required to populate the cardAcct object completely
 
     }
 }
