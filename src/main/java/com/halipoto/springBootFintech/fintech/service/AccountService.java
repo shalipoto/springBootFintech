ba717e0cc9aa4290e81ec7d5680be3f62a0ae5e3
aaminahh <aaminah.halipoto@gmail.com>
2022-09-26 23:04:21 -0500

    added validator code with unit tests. all passed

--- src/main/java/com/halipoto/springBootFintech/fintech/service/AccountService.java
+++ src/main/java/com/halipoto/springBootFintech/fintech/service/AccountService.java
@@ -2,14 +2,13 @@ package com.halipoto.springBootFintech.fintech.service;
 
 import com.halipoto.springBootFintech.fintech.model.CardAcct;
 import com.halipoto.springBootFintech.fintech.model.CcAccount;
-import org.springframework.core.env.SystemEnvironmentPropertySource;
 
 public class AccountService {
 
     /**
      * This method receives a CcAccount retrieved from the backend
      * and then populates the UI card account object with synthetic data
-     * @param cc
+     * @param cc backend
      * @return cardAcct
      * @author AHalipoto
      */
@@ -18,13 +17,14 @@ public class AccountService {
     // take the input CcAccount object, create values and add to the
     // newly created CardAcct object and return the object.
     // you are only required to populate the cardAcct object completely
-    public CardAcct processCreditCardAccountForUi(CcAccount cc){
+    public CardAcct processCreditCardAccountForUi(CcAccount cc) throws Exception {
 
         CardAcct cardAcct = new CardAcct();
 
+        validateCCAccount(cc);
         return processCCAccount(cc, cardAcct);
-    }
 
+    }
     private CardAcct processCCAccount(CcAccount cc, CardAcct cardAcct) {
         cardAcct.setCardProcessor(cc.getCardProcessorPartner());
         if (cc.isAutoPayenrolled())
@@ -42,6 +42,24 @@ public class AccountService {
         cardAcct.setLastStatementBalanceDue(cc.getLastStatementBalanceDue());
 
         return cardAcct;
+    }
+
+    private void validateCCAccount(CcAccount cc) throws Exception { //throws exception for null or empty string
+        if (cc.getCardAccountNumber() == null
+                || cc.getTotalBalanceDue() == null
+                || cc.getLastStatementBalanceDue() == null
+                || cc.getMinimumPaymentDue() == null
+                || cc.getCardProcessorPartner() == null
+                || cc.getRewardsType()== null
+                || cc.getCardStatus() == null){
+            throw new Exception("Values cannot be null");
+        }
+        if (cc.getCardAccountNumber().equals("")
+                || cc.getCardProcessorPartner().equals("")
+                || cc.getRewardsType().equals("")
+                || cc.getCardStatus().equals("")){
+            throw new Exception("String cannot be empty");
+        }
     }
 
 }
--- src/test/java/com/halipoto/springBootFintech/fintech/service/TestAccountService.java
+++ src/test/java/com/halipoto/springBootFintech/fintech/service/TestAccountService.java
@@ -3,6 +3,7 @@ package com.halipoto.springBootFintech.fintech.service;
 import com.halipoto.springBootFintech.fintech.model.CardAcct;
 import com.halipoto.springBootFintech.fintech.model.CcAccount;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import static org.junit.Assert.*;
@@ -11,11 +12,12 @@ import java.math.BigDecimal;
 
 public class TestAccountService {
 
-    @InjectMocks //gives the class you want to test and names it service
+    //@InjectMocks //gives the class you want to test and names it service
     AccountService service = new AccountService(); //MAKE SURE U DO THIS?
+    CardAcct thunder = new CardAcct(); //now making dummy card account
 
-    @Test
-    public void accountProcessingTest (){
+    @Test //needed for every test method
+    public void accountProcessingTest () throws Exception {
         CcAccount newCCAccount = new CcAccount(); //populating 'original' cc account
 
         newCCAccount.setCardAccountNumber("987654321");
@@ -30,8 +32,6 @@ public class TestAccountService {
         newCCAccount.setAutoPayenrolled(false);
         newCCAccount.setChipCard(true);
         newCCAccount.setCardStatus("available");
-
-        CardAcct thunder = new CardAcct(); //now making dummy card account
 
         thunder = service.processCreditCardAccountForUi(newCCAccount); // processing thunder class
 
@@ -52,5 +52,225 @@ public class TestAccountService {
     /** Write a new test method to see what your code does when one or more of the
      *  inputs are null or have empty String values (e.g. "")
      */
+    @Test(expected = Exception.class) // ensuring that test case passes on exception
+    //method throws an exception, finds exception, passes bc it finds exception
+    public void validateCCNumberTest () throws Exception {
+        CcAccount newCCAccount = new CcAccount(); //casting 'original' cc account
+
+        newCCAccount.setCardAccountNumber(null);
+        newCCAccount.setUserName("aaminahh");
+        newCCAccount.setTotalBalanceDue(BigDecimal.valueOf(239349.23));
+        newCCAccount.setLastStatementBalanceDue(BigDecimal.valueOf(495.49));
+        newCCAccount.setMinimumPaymentDue(BigDecimal.valueOf(12.34));
+        newCCAccount.setGracePeriod(12);
+        newCCAccount.setCardProcessorPartner("Comerica");
+        newCCAccount.setProductCode(388595);
+        newCCAccount.setRewardsType("premium");
+        newCCAccount.setAutoPayenrolled(false);
+        newCCAccount.setChipCard(true);
+        newCCAccount.setCardStatus("available");
+
+        thunder = service.processCreditCardAccountForUi(newCCAccount); // processing thunder class
+
+        assertEquals("987654321", thunder.getCcAccountNumber()); // checking each thunder att
+        assertEquals(BigDecimal.valueOf(239349.23), thunder.getTotalBalance());
+        assertEquals(BigDecimal.valueOf(495.49), thunder.getLastStatementBalanceDue());
+        assertEquals(BigDecimal.valueOf(12.34), thunder.getMinimumPaymentAmountDue());
+        assertEquals(12, thunder.getGracePeriod());
+        assertEquals("Comerica", thunder.getCardProcessor());
+        assertEquals("premium", thunder.getRewards());
+        assertEquals("false", thunder.getAutopayIndicator());
+        assertEquals("true", thunder.getChipCardIndicator());
+        assertEquals("available", thunder.getCardStatus());
+
+    }
+    @Test(expected = Exception.class)
+    public void validateCCTotalBalanceDueTest () throws Exception {
+        CcAccount newCCAccount = new CcAccount(); //casting 'original' cc account
+
+        newCCAccount.setCardAccountNumber("123546789");
+        newCCAccount.setUserName("aaminahh");
+        newCCAccount.setTotalBalanceDue(null);
+        newCCAccount.setLastStatementBalanceDue(BigDecimal.valueOf(495.49));
+        newCCAccount.setMinimumPaymentDue(BigDecimal.valueOf(12.34));
+        newCCAccount.setGracePeriod(12);
+        newCCAccount.setCardProcessorPartner("Comerica");
+        newCCAccount.setProductCode(388595);
+        newCCAccount.setRewardsType("premium");
+        newCCAccount.setAutoPayenrolled(false);
+        newCCAccount.setChipCard(true);
+        newCCAccount.setCardStatus("available");
+
+        thunder = service.processCreditCardAccountForUi(newCCAccount); // processing thunder class
+
+        assertEquals("987654321", thunder.getCcAccountNumber()); // checking each thunder att
+        assertEquals(BigDecimal.valueOf(239349.23), thunder.getTotalBalance());
+        assertEquals(BigDecimal.valueOf(495.49), thunder.getLastStatementBalanceDue());
+        assertEquals(BigDecimal.valueOf(12.34), thunder.getMinimumPaymentAmountDue());
+        assertEquals(12, thunder.getGracePeriod());
+        assertEquals("Comerica", thunder.getCardProcessor());
+        assertEquals("premium", thunder.getRewards());
+        assertEquals("false", thunder.getAutopayIndicator());
+        assertEquals("true", thunder.getChipCardIndicator());
+        assertEquals("available", thunder.getCardStatus());
+
+    }
+    @Test(expected = Exception.class)
+    public void validateCCLastStatementDueTest () throws Exception {
+        CcAccount newCCAccount = new CcAccount(); //casting 'original' cc account
+
+        newCCAccount.setCardAccountNumber("123546789");
+        newCCAccount.setUserName("aaminahh");
+        newCCAccount.setTotalBalanceDue(BigDecimal.valueOf(239349.23));
+        newCCAccount.setLastStatementBalanceDue(null);
+        newCCAccount.setMinimumPaymentDue(BigDecimal.valueOf(12.34));
+        newCCAccount.setGracePeriod(12);
+        newCCAccount.setCardProcessorPartner("Comerica");
+        newCCAccount.setProductCode(388595);
+        newCCAccount.setRewardsType("premium");
+        newCCAccount.setAutoPayenrolled(false);
+        newCCAccount.setChipCard(true);
+        newCCAccount.setCardStatus("available");
+
+        thunder = service.processCreditCardAccountForUi(newCCAccount); // processing thunder class
+
+        assertEquals("987654321", thunder.getCcAccountNumber()); // checking each thunder att
+        assertEquals(BigDecimal.valueOf(239349.23), thunder.getTotalBalance());
+        assertEquals(BigDecimal.valueOf(495.49), thunder.getLastStatementBalanceDue());
+        assertEquals(BigDecimal.valueOf(12.34), thunder.getMinimumPaymentAmountDue());
+        assertEquals(12, thunder.getGracePeriod());
+        assertEquals("Comerica", thunder.getCardProcessor());
+        assertEquals("premium", thunder.getRewards());
+        assertEquals("false", thunder.getAutopayIndicator());
+        assertEquals("true", thunder.getChipCardIndicator());
+        assertEquals("available", thunder.getCardStatus());
+
+    }
+    @Test(expected = Exception.class)
+    public void validateCCMinimumPaymentTest () throws Exception {
+        CcAccount newCCAccount = new CcAccount(); //casting 'original' cc account
+
+        newCCAccount.setCardAccountNumber("123546789");
+        newCCAccount.setUserName("aaminahh");
+        newCCAccount.setTotalBalanceDue(BigDecimal.valueOf(239349.23));
+        newCCAccount.setLastStatementBalanceDue(BigDecimal.valueOf(495.49));
+        newCCAccount.setMinimumPaymentDue(null);
+        newCCAccount.setGracePeriod(12);
+        newCCAccount.setCardProcessorPartner("Comerica");
+        newCCAccount.setProductCode(388595);
+        newCCAccount.setRewardsType("premium");
+        newCCAccount.setAutoPayenrolled(false);
+        newCCAccount.setChipCard(true);
+        newCCAccount.setCardStatus("available");
+
+        thunder = service.processCreditCardAccountForUi(newCCAccount); // processing thunder class
+
+        assertEquals("987654321", thunder.getCcAccountNumber()); // checking each thunder att
+        assertEquals(BigDecimal.valueOf(239349.23), thunder.getTotalBalance());
+        assertEquals(BigDecimal.valueOf(495.49), thunder.getLastStatementBalanceDue());
+        assertEquals(BigDecimal.valueOf(12.34), thunder.getMinimumPaymentAmountDue());
+        assertEquals(12, thunder.getGracePeriod());
+        assertEquals("Comerica", thunder.getCardProcessor());
+        assertEquals("premium", thunder.getRewards());
+        assertEquals("false", thunder.getAutopayIndicator());
+        assertEquals("true", thunder.getChipCardIndicator());
+        assertEquals("available", thunder.getCardStatus());
+
+    }
+    @Test(expected = Exception.class)
+    public void validateCCProcessorPartnerTest () throws Exception {
+        CcAccount newCCAccount = new CcAccount(); //casting 'original' cc account
+
+        newCCAccount.setCardAccountNumber("123546789");
+        newCCAccount.setUserName("aaminahh");
+        newCCAccount.setTotalBalanceDue(BigDecimal.valueOf(239349.23));
+        newCCAccount.setLastStatementBalanceDue(BigDecimal.valueOf(495.49));
+        newCCAccount.setMinimumPaymentDue(BigDecimal.valueOf(12.34));
+        newCCAccount.setGracePeriod(12);
+        newCCAccount.setCardProcessorPartner(null);
+        newCCAccount.setProductCode(388595);
+        newCCAccount.setRewardsType("premium");
+        newCCAccount.setAutoPayenrolled(false);
+        newCCAccount.setChipCard(true);
+        newCCAccount.setCardStatus("available");
+
+        thunder = service.processCreditCardAccountForUi(newCCAccount); // processing thunder class
+
+        assertEquals("987654321", thunder.getCcAccountNumber()); // checking each thunder att
+        assertEquals(BigDecimal.valueOf(239349.23), thunder.getTotalBalance());
+        assertEquals(BigDecimal.valueOf(495.49), thunder.getLastStatementBalanceDue());
+        assertEquals(BigDecimal.valueOf(12.34), thunder.getMinimumPaymentAmountDue());
+        assertEquals(12, thunder.getGracePeriod());
+        assertEquals("Comerica", thunder.getCardProcessor());
+        assertEquals("premium", thunder.getRewards());
+        assertEquals("false", thunder.getAutopayIndicator());
+        assertEquals("true", thunder.getChipCardIndicator());
+        assertEquals("available", thunder.getCardStatus());
+
+    }
+    @Test(expected = Exception.class)
+    public void validateCCRewardsTypeTest () throws Exception {
+        CcAccount newCCAccount = new CcAccount(); //casting 'original' cc account
+
+        newCCAccount.setCardAccountNumber("123546789");
+        newCCAccount.setUserName("aaminahh");
+        newCCAccount.setTotalBalanceDue(BigDecimal.valueOf(239349.23));
+        newCCAccount.setLastStatementBalanceDue(BigDecimal.valueOf(495.49));
+        newCCAccount.setMinimumPaymentDue(BigDecimal.valueOf(12.34));
+        newCCAccount.setGracePeriod(12);
+        newCCAccount.setCardProcessorPartner("Comerica");
+        newCCAccount.setProductCode(388595);
+        newCCAccount.setRewardsType(null);
+        newCCAccount.setAutoPayenrolled(false);
+        newCCAccount.setChipCard(true);
+        newCCAccount.setCardStatus("available");
+
+        thunder = service.processCreditCardAccountForUi(newCCAccount); // processing thunder class
+
+        assertEquals("987654321", thunder.getCcAccountNumber()); // checking each thunder att
+        assertEquals(BigDecimal.valueOf(239349.23), thunder.getTotalBalance());
+        assertEquals(BigDecimal.valueOf(495.49), thunder.getLastStatementBalanceDue());
+        assertEquals(BigDecimal.valueOf(12.34), thunder.getMinimumPaymentAmountDue());
+        assertEquals(12, thunder.getGracePeriod());
+        assertEquals("Comerica", thunder.getCardProcessor());
+        assertEquals("premium", thunder.getRewards());
+        assertEquals("false", thunder.getAutopayIndicator());
+        assertEquals("true", thunder.getChipCardIndicator());
+        assertEquals("available", thunder.getCardStatus());
+
+    }
+    @Test(expected = Exception.class)
+    public void validateCCCardStatusTest () throws Exception {
+        CcAccount newCCAccount = new CcAccount(); //casting 'original' cc account
+
+        newCCAccount.setCardAccountNumber("123546789");
+        newCCAccount.setUserName("aaminahh");
+        newCCAccount.setTotalBalanceDue(BigDecimal.valueOf(239349.23));
+        newCCAccount.setLastStatementBalanceDue(BigDecimal.valueOf(495.49));
+        newCCAccount.setMinimumPaymentDue(BigDecimal.valueOf(12.34));
+        newCCAccount.setGracePeriod(12);
+        newCCAccount.setCardProcessorPartner("Comerica");
+        newCCAccount.setProductCode(388595);
+        newCCAccount.setRewardsType("premium");
+        newCCAccount.setAutoPayenrolled(false);
+        newCCAccount.setChipCard(true);
+        newCCAccount.setCardStatus(null);
+
+        //CardAcct thunder = new CardAcct(); //now making dummy card account
+
+        thunder = service.processCreditCardAccountForUi(newCCAccount); // processing thunder class
+
+        assertEquals("987654321", thunder.getCcAccountNumber()); // checking each thunder att
+        assertEquals(BigDecimal.valueOf(239349.23), thunder.getTotalBalance());
+        assertEquals(BigDecimal.valueOf(495.49), thunder.getLastStatementBalanceDue());
+        assertEquals(BigDecimal.valueOf(12.34), thunder.getMinimumPaymentAmountDue());
+        assertEquals(12, thunder.getGracePeriod());
+        assertEquals("Comerica", thunder.getCardProcessor());
+        assertEquals("premium", thunder.getRewards());
+        assertEquals("false", thunder.getAutopayIndicator());
+        assertEquals("true", thunder.getChipCardIndicator());
+        assertEquals("available", thunder.getCardStatus());
+
+    }
 
 }
